a8a152db4ee2e46d65fb87b48b3e28aea8564826
Eman <98251644+emanp@users.noreply.github.com>
2024-09-23 18:30:55 -0700

    kjb

--- portfolio/src/Components/ContactForm.js
+++ portfolio/src/Components/ContactForm.js
@@ -10,9 +10,9 @@ export default function ContactForm()
     const [message, setMessage] = useState("")
     const [email, setEmail] = useState("")
 
-    console.log("Name: " + name);
-    console.log("Message: " + message)
-    console.log("Email: " + email)
+    // console.log("Name: " + name);
+    // console.log("Message: " + message)
+    // console.log("Email: " + email)
 
     const isValidForm = (email !== "" && message !== "" && name !== "") && isValidEmail(email);
 
@@ -33,32 +33,37 @@ export default function ContactForm()
 
     return (
         <div>
-        <h1 style={{marginTop: '100px' , textAlign: 'center'}}>Contact Me!</h1>
-        <p style={{textAlign: 'center'}}>I'd love to hear from you. Send me a message and I'll respond as soon as possible.</p>
-        <div className="form-container">
+            <div>
+                <h1 style={{marginTop: '100px' , textAlign: 'center'}}>Contact Me!</h1>
+                <p style={{textAlign: 'center'}}>I'd love to hear from you. Send me a message and I'll respond as soon as possible.</p>
+            </div>
 
-            <form onSubmit={onSubmit}>
-                <div>
-                    <div><label htmlFor="Name"> Name* </label></div>
-                    <input type="text" name="from_name" onChange={(e) => setName(e.target.value)}/> 
-                </div>
+            <div className="form-container">
 
-                <div>
-                    <div><label htmlFor="email"> Email* </label></div>
-                    <input type="text" name="email_from" onChange={(e) => setEmail(e.target.value)}/>
-                </div>
+                <form onSubmit={onSubmit}>
+                    <div>
+                        <div><label htmlFor="Name"> Name* </label></div>
+                        <input type="text" name="from_name" onChange={(e) => setName(e.target.value)}/> 
+                    </div>
 
-                <div>
-                    <div><label htmlFor="Message"> Message* </label></div>
-                    <input type="text" name="message" onChange={(e) => setMessage(e.target.value)}/>
-                </div>
+                    <div>
+                        <div><label htmlFor="email"> Email* </label></div>
+                        <input type="text" name="email_from" onChange={(e) => setEmail(e.target.value)}/>
+                    </div>
 
-                <div style={{textAlign: "center"}}>
-                    <input type="submit" value="Send Message" disabled={!isValidForm}/>
-                </div>
+                    <div>
+                        <div><label htmlFor="Message"> Message* </label></div>
+                        <textarea name="message" rows="5" cols="32" style={{ fontSize: '1rem', padding: '10px' }} onChange={(e) => setMessage(e.target.value)} />
+                    </div>
 
-            </form>
-        </div>
+                    <div style={{textAlign: "center"}}>
+                        <span className="navbar-text">
+                            <input type="submit" value="Send Message" disabled={!isValidForm}/>
+                        </span>
+                    </div>
+
+                </form>
+            </div>
         </div>
             
     )
